e8e5e03799a4a11f6ccc4920af498a99a8eee634
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2022-09-26 09:26:19 -0400

    add icon from db

--- src/config/img/assets-storage.ts
+++ src/config/img/assets-storage.ts
@@ -1,10 +1,13 @@
- const assets = {
+const assets = {
   public: {
     logo: "./img/logo.png",
     watermarkCopyright: "./img/watermark-copyright.png",
     svg: {
       iconTest: "./img/svg/icon-test.svg",
     },
+    png: {
+      iconBankDefault: "./img/icon-bank.png",
+    },
   },
 };
-export default assets
+export default assets;
